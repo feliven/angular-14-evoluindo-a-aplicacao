c8411cf51fc4a9c17e921c8c7942ea12ac7e4f56
FVS <feliven@gmail.com>
2025-10-17 12:46:17 -0300

    imagem no cabeçalho agora é clicável

--- src/app/components/cabecalho/cabecalho.ts
+++ src/app/components/cabecalho/cabecalho.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cabecalho.html',
-  styleUrl: './cabecalho.css'
+  styleUrl: './cabecalho.css',
 })
 export class Cabecalho {
+  constructor(private router: Router) {}
 
+  vaiParaPaginaInicial() {
+    // console.log('img clicada');
+    this.router.navigate(['/']);
+  }
 }
